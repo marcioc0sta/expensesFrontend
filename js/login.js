14c648e02484966698009d941a0e9c330d4bc8c8
marcio <marciocst2@gmail.com>
2024-10-22 09:59:27 -0300

    fix: api url on login

--- js/login.js
+++ js/login.js
@@ -1,5 +1,4 @@
-// const API_URL = 'http://ec2-50-19-19-195.compute-1.amazonaws.com';
-const API_URL = 'http://localhost:8080';
+const API_URL = 'http://ec2-50-19-19-195.compute-1.amazonaws.com';
 
 // HTML ELEMENTS
 const container = document.getElementById('container');
